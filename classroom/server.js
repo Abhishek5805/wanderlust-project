bcd09556327d636aac62261fc2356272112e0ff4
demo <abhishekshinde5805@gmail.com>
2026-04-01 21:39:39 +0530

    modify readme.md

--- classroom/server.js
+++ classroom/server.js
@@ -5,6 +5,20 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get("/users", (req, res) => {
+    res.send("List of users");
+});
+
+//show routes
+app.get('"/users/:id', (req, res) => {
+    res.send("get for users");
+}
+
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+
+
