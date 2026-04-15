c102400bc1d60795f66c3f60996b40ffa28dccbe
demo <abhishekshinde5805@gmail.com>
2026-04-15 21:46:21 +0530

    adding cookies

--- classroom/server.js
+++ classroom/server.js
@@ -3,61 +3,63 @@ const app = express();
 const users = require('./routes/users.js');
 const posts = require('./routes/posts.js');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-app.use(cookieParser("secretkey"));
+// app.use(cookieParser("secretkey"));
  
-app.get('/getsignedcookie', (req, res) => {
-    res.cookie('name', 'John Doe',{signed: true});
-    res.cookie('age', '30');
-    res.send('Cookie has been set');
-}   );
+// app.get('/getsignedcookie', (req, res) => {
+//     res.cookie('name', 'John Doe',{signed: true});
+//     res.cookie('age', '30');
+//     res.send('Cookie has been set');
+// }   );
 
-app.get('/verifycookie', (req, res) => {
-    console.log(req.signedCookies);
-    res.send('Cookie has been verified');
-});
+// app.get('/verifycookie', (req, res) => {
+//     console.log(req.signedCookies);
+//     res.send('Cookie has been verified');
+// });
 
-app.get('/getcookies', (req, res) => {
-    res.cookie('name', 'John Doe');
-    res.cookie('age', '30');
-    res.send('Cookie has been set');
-});
+// app.get('/getcookies', (req, res) => {
+//     res.cookie('name', 'John Doe');
+//     res.cookie('age', '30');
+//     res.send('Cookie has been set');
+// });
 
-app.get('/greet', (req, res) => {
-    const name = req.cookies.name || 'Guest';
-    res.send(`Hello, ${name}!`);
-});
-app.get('/', (req, res) => {
-    console.dir(req.cookies);
-    res.send('Hello, World!');
-});
+// app.get('/greet', (req, res) => {
+//     const name = req.cookies.name || 'Guest';
+//     res.send(`Hello, ${name}!`);
+// });
+// app.get('/', (req, res) => {
+//     console.dir(req.cookies);
+//     res.send('Hello, World!');
+// });
 
-app.use("/users", users);
+// app.use("/users", users);
 
-//insert routes
-app.get("/posts", (req, res) => {
-    res.send("List of posts");
-});
+// //insert routes
+// app.get("/posts", (req, res) => {
+//     res.send("List of posts");
+// });
 
-//show routes
-app.get("/posts/:id", (req, res) => {
-    res.send("Show a post");
-});
+// //show routes
+// app.get("/posts/:id", (req, res) => {
+//     res.send("Show a post");
+// });
 
-//post routes
-app.post("/posts", (req, res) => {
-    res.send("Create a new post");
-});
+// //post routes
+// app.post("/posts", (req, res) => {
+//     res.send("Create a new post");
+// });
 
-//update routes
-app.put("/posts/:id", (req, res) => {
-    res.send("Update a post");
-});
+// //update routes
+// app.put("/posts/:id", (req, res) => {
+//     res.send("Update a post");
+// });
 
-//delete routes
-app.delete("/posts/:id", (req, res) => {
-    res.send("Delete a post ");
-});
+// //delete routes
+// app.delete("/posts/:id", (req, res) => {
+//     res.send("Delete a post ");
+// });
+
 
 
 app.listen(3000, () => {
